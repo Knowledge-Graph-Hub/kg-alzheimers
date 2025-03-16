f5b1714798ebc69a439bd25a95d99a58954ea84e
caufieldjh <j.harry.caufield@gmail.com>
2025-03-16 17:54:22 -0400

    Update name to kg-alzheimers

--- scripts/kgx_transforms.sh
+++ scripts/kgx_transforms.sh
@@ -9,4 +9,4 @@ mkdir neo4j-v4-data || True
 docker run -d --name neo -p7474:7474 -p7687:7687 -v neo4j-v4-data:/data --env NEO4J_AUTH=neo4j/admin neo4j:4.4
 poetry run kgx transform --transform-config neo4j-transform.yaml > kgx-transform.log
 docker stop neo
-docker run -v $(pwd)/output:/backup -v neo4j-v4-data:/data --entrypoint neo4j-admin neo4j:4.4 dump --to /backup/monarch-kg.neo4j.dump
+docker run -v $(pwd)/output:/backup -v neo4j-v4-data:/data --entrypoint neo4j-admin neo4j:4.4 dump --to /backup/kg-alzheimers.neo4j.dump
